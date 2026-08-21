c382069a33e49313e207dc0cd0179a14fd2d3852
ItsmeLee <66399601+InwooLeeme@users.noreply.github.com>
2026-08-21 09:54:01 +0900

    refactor : 과제 4 - 날씨 컴포넌트를 exercise 폴더로 이동

---
+++ src/components/exercise/BaseDashboardCard.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({ title: String });
+</script>
+
+<template>
+  <section class="dashboard_card">
+    <h1>{{ title }}</h1>
+    <slot></slot>
+  </section>
+</template>
+
+<style scoped>
+.dashboard_card{ 
+    width: 100%;
+    padding: 16px;
+    box-sizing: border-box;
+    background-color: #ffffff;
+    border-radius: 10px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.04);
+
+    h1{
+        font-size: 15px;
+        font-weight: 700;
+        color: #1f2d3d;
+    }
+}
+</style>
