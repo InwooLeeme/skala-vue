4d14887780d68fb6bfef60f10878e0e8288e807f
ItsmeLee <66399601+InwooLeeme@users.noreply.github.com>
2026-08-23 19:54:29 +0900

    feat : api 페이지에 도시 추가

--- src/api/index.js
+++ src/api/index.js
@@ -7,6 +7,12 @@ export const CITIES = [
     { id: 'city_02', name: '수원', query: 'Suwon,KR', lat: 37.2636, lon: 127.0286 },
     { id: 'city_03', name: '부산', query: 'Busan,KR', lat: 35.1796, lon: 129.0756 },
     { id: 'city_04', name: '경주', query: 'Gyeongju,KR', lat: 35.8562, lon: 129.2247 },
+    { id : 'city_05', name : '인천', query: 'Incheon,KR', lat: 37.4563, lon: 126.7052},
+    { id : 'city_06', name : '대전', query: 'Daejeon,KR', lat: 36.3504, lon: 127.3845},
+    { id : 'city_07', name : '대구', query: 'Daegu,KR', lat: 35.8714, lon: 128.6014},
+    { id : 'city_08', name : '광주', query: 'Gwangju,KR', lat: 35.1595, lon: 126.8526},
+    { id : 'city_09', name : '제주', query: 'Jeju,KR', lat: 33.4996, lon: 126.5312},
+    { id : 'city_10', name : '강릉', query: 'Gangneung,KR', lat: 37.7519, lon: 128.8761},
 ];
 
 export const fetchWeatherDataByCoord = async ({ lat, lon }) => {
